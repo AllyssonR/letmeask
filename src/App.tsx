46b783e9fa41174e63eddbe8f4600a632db26655
AllyssonR <42867875+AllyssonR@users.noreply.github.com>
2021-06-24 17:43:11 -0300

    sign in with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import {Home} from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { createContext, useState } from "react";
+import { firebase, auth } from "./services/firebase";
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+type AuthContextProps = {
+  user: User |undefined;
+  SignInWithGoogle: () => void;
+};
+export const AuthContext = createContext({} as AuthContextProps);
 
 function App() {
+  const [user, setUser] = useState<User>();
+
+  function SignInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then((result) => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+        if (!displayName || !photoURL) {
+          throw new Error("Missin information from Google Account");
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <AuthContext.Provider value={{ user, SignInWithGoogle }}>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContext.Provider>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,19 +1,12 @@
 import ilustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
+import {Link} from 'react-router-dom'
 import { Button } from "../components/Button";
-import { useHistory } from "react-router-dom";
-
 import "../styles/auth.scss";
-import { auth, firebase } from "../services/firebase";
 
 //webpack (snowpack,vite,...)
-export function Home() {
-  const history = useHistory();
+export function NewRoom() {
 
-  function handleCreateRoom() {
-    history.push("/rooms/new");
-  }
   return (
     <div id="page-auth">
       <aside>
@@ -28,16 +21,14 @@ export function Home() {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
 
-          <button className="create-room" onClick={handleCreateRoom}>
-            <img src={googleIconImg} alt="google logo" />
-            Crie sua sala com o google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
+         <h2>Criar uma nova sala</h2>
+          
           <form action="">
             <input type="text" placeholder="Digite o cogigo da sala" />
 
             <Button type="submit">Entrar na sala</Button>
           </form>
+          <p>Quer entrar em uma sala existente <Link to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
