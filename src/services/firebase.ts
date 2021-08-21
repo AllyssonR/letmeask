8c577cd5a8bac9f8609671fba4fa4e6572f2b3a5
AllyssonR <42867875+AllyssonR@users.noreply.github.com>
2021-08-21 16:48:57 -0300

    changes

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,18 +1,18 @@
-import firebase from "firebase";
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SANDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const auth = firebase.auth();
-const database = firebase.database();
-
-export { firebase, auth, database };
+export const auth = firebase.auth()
+export const database = firebase.database()
